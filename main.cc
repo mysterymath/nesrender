27326b9967f108844e04e642fa344bfbe2d0f036
Daniel Thornburgh <mysterymath@gmail.com>
2022-11-24 12:54:37 -0800

    Some presentation fixes; still broken.

--- main.cc
+++ main.cc
@@ -1,13 +1,15 @@
 #include <nesdoug.h>
 #include <neslib.h>
 
-// Framebuffers for the previous and next frame, with 2 bits per pixel, in
+#include <string.h>
+
+// Framebuffers for the current and next frame, with 2 bits per pixel, in
 // column major order. Each pixel corresponds to one of the tiles in the NES
 // nametable and controls its color.
-char fb_prev[240];
+char fb_cur[240];
 char fb_next[240];
 
-void random_diffs();
+void mutate();
 void present();
 
 int main() {
@@ -22,30 +24,29 @@ int main() {
     set_vram_update(NULL);
     char pad_t = pad_trigger(0);
     if (pad_t & PAD_A)
-      random_diffs();
+      mutate();
     present();
     gray_line();
   }
 }
 
-void random_diffs() {
-  constexpr char num_updates = 240;
-  for (char i = 0; i < num_updates; ++i) {
-    fb_next[i] = rand8();
-  }
+void mutate() {
+  char color = (fb_next[0] + 1) & 0b11;
+  memset(fb_next, color | color << 2 | color << 4 | color << 6,
+         sizeof(fb_next));
 }
 
-constexpr char max_updates_per_frame = 4;
+constexpr char max_updates_per_frame = 1;
 char vram_buf[max_updates_per_frame * 3 + 1];
 
 __attribute__((noinline)) void present() {
   char x = 0, y = 0, update_idx = 0, updates_left = max_updates_per_frame;
   bool any_updates = false;
   for (char i = 0; i < sizeof(fb_next); ++i) {
-    char prev = fb_prev[i];
+    char cur = fb_cur[i];
     char next = fb_next[i];
 
-    if (next == prev) {
+    if (next == cur) {
       y += 4;
       if (y > 32) {
         y -= 32;
@@ -55,27 +56,29 @@ __attribute__((noinline)) void present() {
     }
 
     for (char j = 0; j < 4; ++j) {
-      char prev_lo = prev & 0b11;
+      char cur_lo = cur & 0b11;
       char next_lo = next & 0b11;
 
-      if (next_lo != prev_lo) {
+      if (next_lo != cur_lo) {
+        if (!updates_left--)
+          goto done;
         any_updates = true;
         unsigned addr = NTADR_A(x, y);
         vram_buf[update_idx++] = addr >> 8;
         vram_buf[update_idx++] = addr & 0xff;
         vram_buf[update_idx++] = next_lo;
-        if (!--updates_left)
-          goto done;
       }
 
-      prev <<= 2;
-      next <<= 2;
+      cur >>= 2;
+      next >>= 2;
       if (++y == 33) {
         y = 0;
         ++x;
       }
     }
-    fb_prev[i] = fb_next[i];
+    fb_cur[i] = fb_next[i];
+    if (!updates_left)
+      break;
   }
 done:
   if (any_updates) {
