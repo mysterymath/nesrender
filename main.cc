471a803afc6b40581f532c05d6f8f3d24f7add8c
Daniel Thornburgh <mysterymath@gmail.com>
2022-11-27 21:40:08 -0800

    DDA wall drawing.

--- main.cc
+++ main.cc
@@ -15,15 +15,15 @@ char fb_b[960];
 char fb_next[960];
 #pragma clang section bss = ""
 
-void render();
-void present();
-
-char x = 15;
-char y_top = 5;
-char y_bot = 25;
+char corner_x = 15;
+char corner_y_top = 5;
+char corner_y_bot = 20;
 bool control_top;
 
 bool still_presenting;
+
+void render();
+void present();
 
 int main() {
   static const char bg_pal[16] = {0x00, 0x11, 0x16, 0x1a};
@@ -41,39 +41,36 @@ int main() {
       char pad_t = pad_trigger(0);
       char pad = pad_state(0);
       if (pad & PAD_LEFT)
-        x = x ? x - 1 : 31;
+        corner_x = corner_x ? corner_x - 1 : 31;
       else if (pad & PAD_RIGHT)
-        x = x == 31 ? 0 : x + 1;
+        corner_x = corner_x == 31 ? 0 : corner_x + 1;
       if (pad & PAD_UP) {
         if (control_top)
-          y_top = y_top ? y_top - 1 : 29;
+          corner_y_top = corner_y_top ? corner_y_top - 1 : 29;
         else
-          y_bot = y_bot ? y_bot - 1 : 29;
+          corner_y_bot = corner_y_bot ? corner_y_bot - 1 : 29;
       } else if (pad & PAD_DOWN) {
         if (control_top)
-          y_top = y_top == 29 ? 0 : y_top + 1;
+          corner_y_top = corner_y_top == 29 ? 0 : corner_y_top + 1;
         else
-          y_bot = y_bot == 29 ? 0 : y_bot + 1;
+          corner_y_bot = corner_y_bot == 29 ? 0 : corner_y_bot + 1;
       }
       if (pad_t & PAD_A)
         control_top = !control_top;
     }
-    gray_line();
     render();
-    gray_line();
     present();
-    gray_line();
   }
 }
 
-void draw_vert_line(char color, char x, char y_top, char y_bot) {
-  memset(fb_next + x * 30 + y_top, color, y_bot - y_top + 1);
-}
+void wall_move_to(char x, char y_top, char y_bot);
+void wall_draw_to(char color, char x, char y_top, char y_bot);
 
 void render() {
-  memset(fb_next, 0, sizeof(fb_next));
-  if (y_top <= y_bot)
-    draw_vert_line(1, x, y_top, y_bot);
+  gray_line();
+  wall_move_to(0, 0, 29);
+  wall_draw_to(1, corner_x, corner_y_top, corner_y_bot);
+  wall_draw_to(2, 33, 0, 29);
 }
 
 void present() {
@@ -116,4 +113,44 @@ done:
     scroll(present_to_nt_b ? 0x100 : 0, 0);
     present_to_nt_b = !present_to_nt_b;
   }
+}
+
+char cur_x;
+char cur_y_top;
+char cur_y_bot;
+
+void wall_move_to(char x, char y_top, char y_bot) {
+  cur_x = x;
+  cur_y_top = y_top;
+  cur_y_bot = y_bot;
+}
+
+void wall_draw_to(char color, char x, char y_top, char y_bot) {
+  char dx = x - cur_x;
+  signed char dy_top = y_top - cur_y_top;
+  signed char dy_bot = y_bot - cur_y_bot;
+  // Values in 8.8 fixed point
+  int m_top = dy_top * 256 / dx;
+  int m_bot = dy_bot * 256 / dx;
+
+  unsigned y_top_fp = cur_y_top << 8;
+  unsigned y_bot_fp = cur_y_bot << 8;
+
+  char *fb_col = &fb_next[cur_x * 30];
+  for (char draw_x = cur_x; draw_x < x; ++draw_x) {
+    for (char y = 0; y < 30; ++y) {
+      if (y < y_top_fp >> 8)
+        fb_col[y] = 3;
+      else if (y <= y_bot_fp >> 8)
+        fb_col[y] = color;
+      else
+        fb_col[y] = 0;
+    }
+    fb_col += 30;
+    y_top_fp += m_top;
+    y_bot_fp += m_bot;
+  }
+  cur_x = x;
+  cur_y_top = y_top;
+  cur_y_bot = y_bot;
 }
