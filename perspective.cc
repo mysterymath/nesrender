a6ff64e4522620e49ba2389644aedc468776b538
Daniel Thornburgh <mysterymath@gmail.com>
2022-12-11 14:08:16 -0800

    More perspective renderer.

--- perspective.cc
+++ perspective.cc
@@ -37,14 +37,9 @@ static void move_to(uint16_t x, uint16_t y) {
   player.to_vc_z(wall_top_z, &vc_z_top);
   player.to_vc_z(wall_bot_z, &vc_z_bot);
   if (in_frustum(cur_vc_x, cur_vc_y, vc_z_top, vc_z_bot)) {
-
     uint8_t sx, sy_top, sy_bot;
     to_screen(cur_vc_x, cur_vc_y, vc_z_top, vc_z_bot, &sx, &sy_top, &sy_bot);
-#if 0
-    uint16_t sx, sy;
-    to_screen(perspective_cur_vc_x, perspective_cur_vc_y, &sx, &sy);
-    line_move_to(sx, sy);
-#endif
+    wall_move_to(sx, sy_top, sy_bot);
   }
 }
 
@@ -59,5 +54,18 @@ static bool in_frustum(int16_t vc_x, int16_t vc_y, int16_t vc_z_top,
   // TODO: Z
   if (vc_x <= 0)
     return false;
-  return abs(vc_y) <= vc_x;
+  // Using a FOV of 90 degrees, the frustum left and right are defined by y =
+  // +-x.
+  if (abs(vc_y) > vc_x)
+    return false;
+
+  // Since the frustum left and right are y = +-x, take y = x = w/2. This should
+  // lead to a world rectangle of width w and height h, so z must be h/2. But z
+  // is proportional to x, so the frustum top and bottom must be z =
+  // +-(h/2)x/(w/2) = +- hx/w. So, z is in frustum iff abs(z) <= hx/w, or
+  // abs(z)*w <= hx.
+
+  // BUT, this doesn't work! The top could be in frustum, but the bottom isn't,
+  // or vice versa!
+  return true;
 }
