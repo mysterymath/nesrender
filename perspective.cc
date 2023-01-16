71ea283862edf63e0e375307ccd0a01cc097f978
Daniel Thornburgh <mysterymath@gmail.com>
2023-01-16 13:18:01 -0800

    Clip top against top.

--- perspective.cc
+++ perspective.cc
@@ -240,17 +240,75 @@ __attribute__((noinline)) static void draw_to(uint16_t x, uint16_t y) {
       cur_sy_top = 0;
       cur_top_above_top = top_above_top = false;
       DEBUG("Clipped top: both sides.\n");
-    } else if (cur_top_above_top || top_above_top) {
-      // TODO
-    } else if (cur_top_below_bot || top_below_bot) {
-      // TODO
-    } else {
-      lm_top = Log(lsy_top * iscale - lcur_sy_top * iscale) / m_denom;
-      m_top = lm_top * Log::pow2(8);
-      cur_sy_top = lcur_sy_top * lh_over_2_times_256 + screen_height / 2 * 256;
       clip_bot_and_draw(lcur_sx, cur_sx, cur_sy_top, lm_top, m_top, lcur_sy_bot,
                         lsx, sx, lsy_bot, cur_bot_below_bot, bot_below_bot,
                         cur_bot_above_top, bot_above_top);
+    } else {
+      lm_top = Log(lsy_top * iscale - lcur_sy_top * iscale) / m_denom;
+      m_top = lm_top * Log::pow2(8);
+
+      if (cur_top_above_top || top_above_top || cur_top_below_bot ||
+          top_below_bot) {
+        int16_t dx = cc_x - cur_cc_x;
+        int16_t dy_top = cc_y_top - cur_cc_y_top;
+        int16_t dy_bot = cc_y_bot - cur_cc_y_bot;
+        int16_t dw = cc_w - cur_cc_w;
+        Log ldx = dx;
+        Log ldy_top = dy_top;
+        Log ldy_bot = dy_bot;
+        Log ldw = dw;
+
+        if (cur_top_above_top || top_above_top) {
+          // r(t) = cur + vt
+          // r(t)_y = -r(t)_w
+          // cur_y + dyt = -cur_w - dw*t;
+          // t*(dy + dw) = -cur_w - cur_y
+          // t = (-cur_w - cur_y) / (dy + dw)
+          Log t = Log(-cur_cc_w - cur_cc_x) / Log(dy_top + dw);
+          int16_t isect_cc_x = cur_cc_x + ldx * t;
+          int16_t isect_cc_y_top = cur_cc_y_top + ldy_top * t;
+          int16_t isect_cc_y_bot = cur_cc_y_bot + ldy_bot * t;
+          int16_t isect_cc_w = -isect_cc_y_top;
+          bool isect_bot_below_bot = isect_cc_y_bot > isect_cc_w;
+          bool isect_bot_above_top = isect_cc_y_bot < -isect_cc_w;
+          Log lisect_cc_x = isect_cc_x;
+          Log lisect_cc_y_bot = isect_cc_y_bot;
+          Log lisect_cc_w = isect_cc_w;
+          Log lisect_sx = lisect_cc_x / lisect_cc_w;
+          Log lisect_sy_bot = isect_cc_y_bot / lisect_cc_w;
+          uint16_t isect_sx =
+              lisect_sx * Log::pow2(13) + screen_width / 2 * 256;
+          if (cur_top_above_top) {
+            clip_bot_and_draw(lcur_sx, cur_sx, 0, Log::zero(), 0, lcur_sy_bot,
+                              lisect_sx, isect_sx, lisect_sy_bot,
+                              cur_bot_below_bot, isect_bot_below_bot,
+                              cur_bot_above_top, isect_bot_above_top);
+            clip_bot_and_draw(lisect_sx, isect_sx, 0, lm_top, m_top,
+                              lisect_sy_bot, lsx, sx, lsy_bot,
+                              isect_bot_below_bot, bot_below_bot,
+                              isect_bot_above_top, bot_above_top);
+          } else {
+            cur_sy_top =
+                lcur_sy_top * lh_over_2_times_256 + screen_height / 2 * 256;
+            clip_bot_and_draw(lcur_sx, cur_sx, cur_sy_top, lm_top, m_top,
+                              lcur_sy_bot, lisect_sx, isect_sx, lisect_sy_bot,
+                              cur_bot_below_bot, isect_bot_below_bot,
+                              cur_bot_above_top, isect_bot_above_top);
+            clip_bot_and_draw(lisect_sx, isect_sx, 0, Log::zero(), 0,
+                              lisect_sy_bot, lsx, sx, lsy_bot,
+                              isect_bot_below_bot, bot_below_bot,
+                              isect_bot_above_top, bot_above_top);
+          }
+        } else {
+          // TODO
+        }
+      } else {
+        cur_sy_top =
+            lcur_sy_top * lh_over_2_times_256 + screen_height / 2 * 256;
+        clip_bot_and_draw(lcur_sx, cur_sx, cur_sy_top, lm_top, m_top,
+                          lcur_sy_bot, lsx, sx, lsy_bot, cur_bot_below_bot,
+                          bot_below_bot, cur_bot_above_top, bot_above_top);
+      }
     }
   }
 
