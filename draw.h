d3cd1c8b7fcf59361d3a7b351fd55270540a67e0
Daniel Thornburgh <mysterymath@gmail.com>
2023-01-18 22:21:59 -0800

    Remove wall.

--- draw.h
+++ draw.h
@@ -11,8 +11,4 @@ constexpr uint16_t screen_guard = 5 * 256;
 void line_move_to(uint16_t x, uint16_t y);
 void line_draw_to(uint8_t color, uint16_t x, uint16_t y);
 
-// Values are actual pixel locations.
-void wall_move_to(uint16_t x, uint16_t y_top, uint16_t y_bot);
-void wall_draw_to(uint8_t color, uint16_t x, uint16_t y_top, uint16_t y_bot);
-
 #endif // not DRAW_H
