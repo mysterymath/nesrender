6d44ae950ac4d2c8afbb29772783e683c2b9f9f1
Daniel Thornburgh <mysterymath@gmail.com>
2023-02-07 21:50:38 -0800

    Add 0x; tracing works with llvm-symbolizer!

--- trace.lua
+++ trace.lua
@@ -9,7 +9,7 @@ function callback(address, value)
   if address < 0xc000 then
     vma = vma | bank << 16
   end
-  io.write(string.format("%08x\n", vma))
+  io.write(string.format("0x%08x\n", vma))
   ::continue::
 end
 emu.addMemoryCallback(callback, emu.memCallbackType.cpuExec, 0)
