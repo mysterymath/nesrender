9a287bab46b863d31f0539955ecfb9f94000fb2a
Daniel Thornburgh <mysterymath@gmail.com>
2023-02-13 15:30:38 -0800

    Print cycle counts for each instr.

--- trace.lua
+++ trace.lua
@@ -1,15 +1,25 @@
 file = io.open("trace", "w")
 io.output(file)
 
+
+vma = 0
+prevCycleCount = 0
 function callback(address, value)
+  cycleCount = emu.getState().cpu.cycleCount
   prg = emu.getPrgRomOffset(address)
-  if prg == -1 then goto continue end
+  if vma ~= 0 then
+    io.write(string.format("0x%08x %d\n", vma, cycleCount - prevCycleCount))
+  end
+  if prg == -1 then
+    vma = 0
+    goto continue
+  end
   bank = prg // 0x4000
   vma = address
   if address < 0xc000 then
     vma = vma | bank << 16
   end
-  io.write(string.format("0x%08x\n", vma))
+  prevCycleCount = cycleCount
   ::continue::
 end
 emu.addMemoryCallback(callback, emu.memCallbackType.cpuExec, 0)
