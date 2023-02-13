cf1215146cfd33f8270013b38c07c9b3aad4de15
Daniel Thornburgh <mysterymath@gmail.com>
2023-02-12 21:53:58 -0800

    Sort trace by address too.

--- trace.sh
+++ trace.sh
@@ -1,3 +1,3 @@
-sort < ~/.config/mesen/trace | uniq -c | sort -rnk1 > /tmp/addr_counts
+sort < ~/.config/mesen/trace | uniq -c | sort -rnk1,2 > /tmp/addr_counts
 awk '{ print $2 }' < /tmp/addr_counts | ~/mos-bin/llvm-symbolizer --obj render.nes.elf > /tmp/symbols
 awk '{ print $1, $2; do { getline < "/tmp/symbols"; print } while(NF) }' < /tmp/addr_counts
