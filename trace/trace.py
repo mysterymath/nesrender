9c29da1601195dfff68d135f79c828ab5ae07c62
Daniel Thornburgh <mysterymath@gmail.com>
2023-02-14 15:57:41 -0800

    Generate the proto.

--- trace/trace.py
+++ trace/trace.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
+
 import profile_pb2
 import sys
 
+locations = set()
+
+profile = profile_pb2.Profile()
 with open(sys.argv[1]) as file:
     for line in file:
         [addr, count] = line.strip().split()
-        print(f'({addr}, {count})')
+        addr = int(addr, 0)
+        count = int(count, 0)
+        sample = profile.sample.add()
+        sample.location_id.append(addr)
+        sample.value.append(count)
+        locations.add(addr)
 
+for l in locations:
+    loc = profile.location.add()
+    loc.id = loc.address = l
+
+with open('trace.profile', 'wb') as file:
+    file.write(profile.SerializeToString())
