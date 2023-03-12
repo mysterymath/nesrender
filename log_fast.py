4d5edde6e6175d3d1b028fc50514bd2691751b81
Daniel Thornburgh <mysterymath@gmail.com>
2023-03-12 16:37:05 -0700

    Prototype good, fast approximation of log_to_lin.

--- log_fast.py
+++ log_fast.py
@@ -10,20 +10,26 @@ logt_coarse = [0]+[round(math.log2(n*256)*2048) for n in range(1,128)]
 logt_fine = [round(np.mean([math.log2(hi*256+lo)*2048 - logt_coarse[hi] for hi in range(1, 128)])) for lo in range(256)]
 #print(logt_fine)
 
-alogt = [round(2**(n*8/2048)) for n in range(4096)]
-#print(alogt)
-
 def lin_to_log(n):
-    if n // 256 == 0:
-        return logt_small[n % 256]
-    return logt_coarse[n // 256] + logt_fine[n % 256]
+    #if n // 256 == 0:
+    #    return logt_small[n % 256]
+    #return logt_coarse[n // 256] + logt_fine[n % 256]
+    return round(math.log2(n)*2048) if n else -32768
+
+# Extracting out the high bit of w as an 8-bit shift afterwards lets us table
+# the remaining 3 bits of w with 5 bits of 8.
+alogt = [round(2**(i / 32)*256) for i in range(256)]
+print(alogt)
 
 def log_to_lin(n):
     if n < -1.5*2048:
         return 0
     if n <= 0:
         return 1
-    return alogt[n // 8]
+    if n < 8 * 2048:
+      return alogt[n >> 6] >> 8
+    n -= 8 * 2048
+    return alogt[n >> 6]
 
 def approx(n):
     l = lin_to_log(n)
