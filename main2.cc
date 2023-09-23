389f605923d0508c854552db58e418d79d54fff9
Daniel Thornburgh <mysterymath@gmail.com>
2023-09-23 12:14:33 -0700

    Initialize framebuffer

--- main2.cc
+++ main2.cc
@@ -3,6 +3,10 @@
 #include <peekpoke.h>
 #include <stdint.h>
 
+// Configure for SNROM MMC1 board.
+MAPPER_CHR_ROM_KB(8);
+MAPPER_PRG_RAM_KB(8);
+
 typedef uint16_t u16;
 typedef uint8_t u8;
 
@@ -27,15 +31,36 @@ static void ppu_set_addr(u16 addr) {
 
 volatile uint8_t c;
 
-constexpr u16 MMC1_CTRL = 0x8000;
+constexpr u16 mmc1_ctrl = 0x8000;
+constexpr u16 bg_pals = 0x3f00;
 
-constexpr u16 BG_PALS = 0x3f00;
+constexpr u8 frame_buffer_stride = 2+3; // LDA imm, STA abs
+extern u8 frame_buffer[frame_buffer_stride * 960 + 1];
+
+static void init_framebuffer() {
+
+  u16 tile = 0;
+  for (u16 i = 0; i < sizeof(frame_buffer);) {
+    static constexpr u8 lda_imm = 0xa9;
+    frame_buffer[i++] = lda_imm;
+    frame_buffer[i++] = tile++;
+
+    static constexpr u8 sta_abs = 0x8d;
+    frame_buffer[i++] = sta_abs;
+    frame_buffer[i++] = reinterpret_cast<u16>(&PPU.vram.data) & 0xff;
+    frame_buffer[i++] = reinterpret_cast<u16>(&PPU.vram.data) >> 8;
+  }
+  static constexpr u8 rts = 0x60;
+  frame_buffer[sizeof(frame_buffer) - 1] = rts; // RTS
+}
 
 int main() {
+  init_framebuffer();
+
   static const uint8_t bg_pal[16] = {0x0f, 0x06, 0x16, 0x0c};
-  mmc1_register_write(MMC1_CTRL, 0b01100);
+  mmc1_register_write(mmc1_ctrl, 0b01100);
   PPU.control = 0b00001000;
-  ppu_set_addr(BG_PALS);
+  ppu_set_addr(bg_pals);
   for (u16 i = 0; i < sizeof(bg_pal); i++)
     PPU.vram.data = bg_pal[i];
 
