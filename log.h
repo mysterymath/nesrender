4b9fec863feff6228b8604ba2ab2a49542dbf9bd
Daniel Thornburgh <mysterymath@gmail.com>
2023-10-12 10:29:05 -0700

    Constexpr log initializer

--- log.h
+++ log.h
@@ -1,6 +1,7 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include "gcem.hpp"
 #include <stdint.h>
 
 struct Log {
@@ -9,7 +10,9 @@ struct Log {
 
   Log() = default;
   Log(int16_t val);
+  template <typename T> Log(T val) : Log(static_cast<int16_t>(val)) {}
   constexpr Log(bool sign, int16_t exp) : sign(sign), exp(exp){};
+  explicit constexpr Log(float val);
 
   static constexpr Log pow2(uint8_t k) { return Log(false, k << 11); }
   static constexpr Log zero() { return Log(false, -32768); }
@@ -31,5 +34,23 @@ struct Log {
   bool operator==(const Log &other) const;
   bool operator!=(const Log &other) const;
 };
+
+constexpr Log::Log(float val) : sign(false), exp(0) {
+  if (val == 0) {
+    exp = -32768;
+    return;
+  }
+
+  if (val < 0) {
+    val = -val;
+    sign = true;
+  }
+
+  float fexp = gcem::round(gcem::log2(val) * 2048);
+  if (fexp < -32768)
+    fexp = -32768;
+  if (fexp >= 15 * 2048)
+    fexp = 15 * 2048 - 1;
+}
 
 #endif // not LOG_H
